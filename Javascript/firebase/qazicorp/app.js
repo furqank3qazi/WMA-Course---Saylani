3ec5b231e1ae7b70f121633396c11d95126c2e24
Furqan Qazi <furqank3qazi@gmail.com>
2024-02-21 22:10:55 +0500

    final Href updated
    final Href updated

--- Javascript/firebase/qazicorp/app.js
+++ Javascript/firebase/qazicorp/app.js
@@ -70,7 +70,7 @@ const getAllCategories = async () => {
 
 onAuthStateChanged(auth, (user)=>{
   if(user ||
-    location.pathname.indexOf("add-product-category") !== -1 || location.pathname === "/")
+    location.pathname.indexOf("add-product-category") !== -1 || location.pathname === "/" || location.pathname === "/javascript/firebase/qazicorp/")
     {
       console.log(location.pathname)
       console.log("running")
